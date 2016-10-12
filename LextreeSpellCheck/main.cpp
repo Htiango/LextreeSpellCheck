263f5d14a9a12bfdbcc4fffd89c54e1fc3917862
Htiango <hongty106@gmail.com>
2016-10-12 19:46:49 +0800

    Initial Commit

---
+++ LextreeSpellCheck/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LextreeSpellCheck
+//
+//  Created by hty on 12/10/2016.
+//  Copyright © 2016 hty. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
